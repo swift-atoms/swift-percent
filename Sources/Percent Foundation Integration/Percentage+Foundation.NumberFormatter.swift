f0423eec175755e005beb71aa16f811d5926adc9
Coen ten Thije Boonkkamp <coen@coenttb.com>
2026-09-07 21:56:02 +0200

    Remove development documentation

--- Sources/Percent Foundation Integration/Percentage+Foundation.NumberFormatter.swift
+++ Sources/Percent Foundation Integration/Percentage+Foundation.NumberFormatter.swift
@@ -2,7 +2,7 @@ public import Foundation
 public import Percent
 
 extension Percentage {
-    /// Formats the percentage using Foundation's locale-aware percent style.
+
     public func localizedDescription(locale: Locale = .current) -> String {
         let formatter = NumberFormatter()
         formatter.locale = locale
